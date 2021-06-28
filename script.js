6119d60326e546085f31be7fce8e18c95d3c6a68
papay88 <papay88@gmail.com>
2021-06-28 20:18:16 +0300

    Первый коммит

---
+++ script.js
@@ -0,0 +1,41 @@
+'use strict';
+
+let a = 5;
+console.log(a);
+
+let persone = {
+    name: "Pavel",
+    age: "33",
+    isMarried: false
+};
+
+console.log(persone["name"]);
+
+let arr = ['plum.gif','apple.jpg','raz.png'];
+
+console.log(arr[1]);
+
+// alert("hello word");
+
+// let answer = confirm("Pizduk?");
+
+// console.log(answer);
+
+// let answer = +prompt("Pizduk?", "Да");
+
+// console.log(typeof(answer));
+
+// console.log(4 + " - object");
+
+let incr = 10,
+    decr = 10;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log("2" === 2);
+
+let isChecked = true,
+    isClose = false;
+
+console.log(isChecked || isClose);
